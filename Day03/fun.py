e747fb706685305dee770b2157ea055d89c184b3
akannshu <akanshuraj@gmail.com>
2020-05-21 11:03:45 +0530

    updated Day03 codes

---
+++ Day03/fun.py
@@ -0,0 +1,57 @@
+# def length(corona):
+#     count = 0
+#     for i in corona:
+#         count+=1
+
+#     return count    
+
+# # ls = [1,3,5,56,86,23,898,0,27,90]
+
+# # for i in range(length(ls)):
+# #     print(ls[i])
+
+# ls1 = [1,3,5,56,86,23,898,0,27,90,49, 75, 90,77]
+# print(length(ls1))
+# ls2 = [1,3,5,56,86,23,898,0,27,90,"number"]
+# print(length(ls2))
+
+# def minimum(kunal):
+#     mini = 10000
+#     for i in kunal:
+#         if i < mini:
+#             mini = i
+
+#     return mini        
+
+# ls = [1,3,5,56,86,23,898,0,27,90]
+# # print(minimum(ls))
+# print(min(ls))
+
+# ls = [1,3,5,56,86,23,898,0,27,90]
+
+# sums = 0
+
+# for i in ls:
+#     sums+=i
+
+# print(sums)    
+
+# print(sum(ls))
+
+# print(2**10)
+# print(pow(2,10))
+
+# def power(value1, value2):
+#     return value1**value2
+
+# print(power(2,10))    
+
+# s = input()
+# print(s)
+# print(s.split())
+
+
+ls = [3,5,8,9,12,4]
+
+ls = [3,4,5,8,9,12]
+
---
+++ Day03/list.py
@@ -0,0 +1,95 @@
+# student1 = "Debraj"
+# Student2 = "Kriti"
+# student3 = "Kunal"
+# .
+# .
+# .
+# .
+# .
+# .
+# .
+# student60 = "Vaishnavi"
+
+# ls = ["Debraj", "Kriti", "Kunal", . . . . . "Vaishnavi"]
+
+# ls = ["name", True , 66.7, 23]
+# print(ls)
+# print(type(ls))
+
+# ls = ["Name", ["place", 70], True]
+# print(ls)
+
+"""
+ls = ["Debraj", "Kriti", "Kunal", "Vaishnavi", "Akanshu", "Neeraj", "Vinay"]
+
+        0          1        2          3            4         5         6
+        -7        -6       -5         -4           -3        -2        -1
+
+"""        
+ls = [False, "Debraj", "Kriti", "Kunaaal", 99.9, "Vaishnavi", "Akanshu", True, "Neeraj", 78, "Vinay", 45, "Saksham"]
+
+# print(ls[2])
+# print(ls[-2])
+# print(ls[7])
+# print(ls[-6])
+
+# print(ls[4], ls[-4])
+
+# print(ls[10], ls[6])
+# print(ls)
+# ls[3] = "Kunal"
+# print(ls)
+
+# ls[-6] = "Thor"
+
+# ls.sort()
+# print(ls)
+
+# ls.append(999)
+# print(ls)
+
+# ls.insert(2,"Cheetah")
+# print(ls)
+
+# del(ls[-9])
+# print(ls)
+
+# print(ls[3:9])
+# print(ls[3:])
+
+# print(ls[:9])
+
+# print(ls[:])
+
+ls = [1,3,5,56,86,23,898,0,27,90]
+
+# count = 0
+# for i in ls:
+#     count+=1
+
+# print(count)    
+
+# for i in range(len(ls)):
+#     print(ls[i])
+
+# maximum = -1
+# for i in ls:
+#     if maximum < i:
+#         maximum = i
+
+# print(maximum)        
+# print(max(ls))
+
+
+# for i in ls:
+#     print(ls)
+
+# for i in ls:
+#     if i < ls:
+#         print(i)
+"""
+10 times list print -> Shiyan, Kunal, Omesh, Kriti, Meghna, 
+
+values of i --> Shiyan, 
+
+"""
