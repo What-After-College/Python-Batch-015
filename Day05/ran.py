cb7298673b8fdf2ee1d7f93c9fcbbb96f7115590
akannshu <akanshuraj@gmail.com>
2020-05-22 12:06:20 +0530

    Added Day05 random word function

---
+++ Day05/ran.py
@@ -0,0 +1,34 @@
+from random import randint
+
+"""
+randint(2,10000)
+
+
+2
+98
+56
+7
+7
+
+"""
+
+# k = randint(2,100)
+# print(k)
+
+"""momos --> m o m o s"""
+
+# avengers = ["Hulk", "Thor", "Iron Man", "Spider Man", "Captain America", "Black Widow", "Doctor Strange", "Ant Man", "Black Panther", "Falcon", "Winter Solider", "Vision"]
+
+# l = len(avengers)
+# k = randint(0,l-1)
+# print(avengers[k])
+# s=""
+# for i in avengers[k]:
+#     if i == ' ' or i=='a' or i=='o' or i=='e'or i=='i' or i=='u':
+#         s+=i
+#         s+=" "
+#         continue
+      
+#     s+="_ "
+# print(s)
+
