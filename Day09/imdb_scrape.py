18c3e64575ab8396088f62f6f723605ccb67f4c0
Akanshu <akanshuraj@gmail.com>
2020-05-29 13:27:35 +0530

    Update imdb_scrape.py

--- Day09/imdb_scrape.py
+++ Day09/imdb_scrape.py
@@ -1,52 +1,49 @@
-from bs4 import BeautifulSoup
-import pandas as pd
 import json
+import urllib.request
+from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
-# df = pd.read_csv('links.csv')
-# print(df.head())
 
-def get_movie_ids(num=30, page=3):
-    links_data  = pd.read_csv('links.csv')
+def get_movie_ids(num=30, page=1):
+    links_data = pd.read_csv("./scrape_imdb/links.csv")
     movie_ids = list(links_data.imdbId)
-    start_index = (page - 1)*num
+    start_index = (page - 1) * num
     end_index = start_index + num
     return movie_ids[start_index:end_index]
 
-def scrape_movie_index_page(movie_id):
-    url = "https://www.imdb.com/title/tt{}/".format(str(movie_id).zfill(7))
-    current_page = requests.get(url)
+
+def scrape_index_page(movie_id):
+    movie_index_url = "https://www.imdb.com/title/tt{}/".format(str(movie_id).zfill(7))
+    current_page = requests.get(movie_index_url)
     index_soup = BeautifulSoup(current_page.text, "html.parser")
     current_page_json = index_soup.find("script", attrs={"type": "application/ld+json"})
     current_page_json = str(current_page_json)[str(current_page_json).find('{'):-9]
     return current_page_json
-  
 
-def collect_movie_data(movie_id):
-    page_json = json.loads(scrape_movie_index_page(movie_id))
+
+def collect_movie_dict(movie_id):
+    page_json = json.loads(scrape_index_page(movie_id))
     movie = {
         "name": page_json["name"],
         "genre": page_json["genre"],
         "image": page_json["image"],
-        "description": page_json["description"]
+        "description": page_json["description"],
     }
-
     print(movie["name"])
     return movie
-    
+
+
+def get_movies_paged(page=2, movies_per_page=10):
+    ids = get_movie_ids(num=movies_per_page, page=page)
+    scrape_result = {"movies": []}
+    for id in ids:
+        scrape_result["movies"].append(collect_movie_dict(id))
+    return scrape_result
+
 
 if __name__ == "__main__":
     ids = get_movie_ids(10)
-    # print(ids)
-    for x in ids:
-        collect_movie_data(x)
-    
-
-# [114709, 113497, 113228, 114885, 113041, 113277, 114319, 112302, 114576, 113189]
-
-"""
-6 digit id was not available
-1
-0000001
-0114709
-"""
+    print(ids)
+    for id in ids:
+        collect_movie_dict(id)
