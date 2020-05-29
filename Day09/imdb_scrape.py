4a98247f7ea09191fca11874095b87681605481a
akannshu <akanshuraj@gmail.com>
2020-05-29 12:29:45 +0530

    Templates and static folder has been created

--- Day09/imdb_scrape.py
+++ Day09/imdb_scrape.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
 import pandas as pd
-import simplejson
+import json
 import requests
 
 # df = pd.read_csv('links.csv')
 # print(df.head())
 
-def get_movie_ids(num=30, page=1):
+def get_movie_ids(num=30, page=3):
     links_data  = pd.read_csv('links.csv')
     movie_ids = list(links_data.imdbId)
     start_index = (page - 1)*num
@@ -17,30 +17,22 @@ def scrape_movie_index_page(movie_id):
     url = "https://www.imdb.com/title/tt{}/".format(str(movie_id).zfill(7))
     current_page = requests.get(url)
     index_soup = BeautifulSoup(current_page.text, "html.parser")
-    current_page_json = index_soup.find("script", attrs = {"type": "application/ld+json"})
+    current_page_json = index_soup.find("script", attrs={"type": "application/ld+json"})
     current_page_json = str(current_page_json)[str(current_page_json).find('{'):-9]
     return current_page_json
   
 
 def collect_movie_data(movie_id):
-    page_json = simplejson.loads(scrape_movie_index_page(movie_id))
+    page_json = json.loads(scrape_movie_index_page(movie_id))
     movie = {
         "name": page_json["name"],
         "genre": page_json["genre"],
         "image": page_json["image"],
-        "decription": page_json["description"]
-        
+        "description": page_json["description"]
     }
+
     print(movie["name"])
     return movie
-    
-def get_movies_paged(page=1, movie_per_page=10):
-    ids = get_movie_ids(num=movie_per_page, page=page)
-    scrape_result = {"movies":[]}
-    for id in ids:
-        scrape_result["movies"].append(collect_movie_data(id))
-
-    return scrape_result    
     
 
 if __name__ == "__main__":
---
+++ Day10/scrape_imdb/imdb_scrape.py
@@ -0,0 +1,49 @@
+import json
+import urllib.request
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+
+def get_movie_ids(num=30, page=1):
+    links_data = pd.read_csv("links.csv")
+    movie_ids = list(links_data.imdbId)
+    start_index = (page - 1) * num
+    end_index = start_index + num
+    return movie_ids[start_index:end_index]
+
+
+def scrape_index_page(movie_id):
+    movie_index_url = "https://www.imdb.com/title/tt{}/".format(str(movie_id).zfill(7))
+    current_page = requests.get(movie_index_url)
+    index_soup = BeautifulSoup(current_page.text, "html.parser")
+    current_page_json = index_soup.find("script", attrs={"type": "application/ld+json"})
+    current_page_json = str(current_page_json)[str(current_page_json).find('{'):-9]
+    return current_page_json
+
+
+def collect_movie_dict(movie_id):
+    page_json = json.loads(scrape_index_page(movie_id))
+    movie = {
+        "name": page_json["name"],
+        "genre": page_json["genre"],
+        "image": page_json["image"],
+        "description": page_json["description"],
+    }
+    print(movie["name"])
+    return movie
+
+
+def get_movies_paged(page=1, movies_per_page=5):
+    ids = get_movie_ids(num=movies_per_page, page=page)
+    scrape_result = {"movies": []}
+    for id in ids:
+        scrape_result["movies"].append(collect_movie_dict(id))
+    return scrape_result
+
+
+if __name__ == "__main__":
+    ids = get_movie_ids(10)
+    print(ids)
+    for id in ids:
+        collect_movie_dict(id)
